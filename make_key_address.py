24d058d0d653b42ff59a9ae6de1fcfb5db8a380b
Wichit Saiklao <wichit@dxplace.com>
2015-05-20 11:11:31 +0700

    create make_key_address.py

---
+++ make_key_address.py
@@ -0,0 +1,20 @@
+import ecdsa
+import bitcoin
+
+# Generate a random private key
+valid_private_key = False
+while not valid_private_key:
+    private_key = bitcoin.random_key()
+    decoded_private_key = bitcoin.decode_privkey(private_key, 'hex')
+    valid_private_key = 0 < decoded_private_key < bitcoin.N
+
+print "Private Key (hex) is: ", private_key
+print "Private Key (decimal) is: ", decoded_private_key
+
+# Convert private key to WIF format
+wif_encoded_private_key = bitcoin.encode_privkey(decoded_private_key, 'wif')
+print "Private Key (WIF) is: ", wif_encoded_private_key 
+
+# Add suffix "01" to indicate a compressed private key
+compressed_private_key = private_key + '01'
+print "Private Key Compressed (hex) is: ", compressed_private_key
